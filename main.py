d7e4c4cf522d67679cb6d04aeda93852e53359b4
BenMac31 <117536235+BenMac31@users.noreply.github.com>
2023-02-18 18:22:49 -0500

    Housekeeping

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
@@ -12,5 +12,5 @@ cm = 1
 
 A = LargeMotor(OUTPUT_A)
 A.on_for_rotations(SpeedPercent(75), rotPercm*cm)
-B = LargeMotor(OUTPUT_B)
-B.on_for_rotations(SpeedPercent(75), rotPercm*cm)
+D = LargeMotor(OUTPUT_D)
+D.on_for_rotations(SpeedPercent(75), rotPercm*cm)
