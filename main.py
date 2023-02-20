baab934c6e37391837e037a0d8439d5b11251801
BenMac31 <117536235+BenMac31@users.noreply.github.com>
2023-02-20 02:08:41 -0500

    Quick push

--- main.py
+++ main.py
@@ -8,38 +8,20 @@ from ev3dev2.sensor.lego import TouchSensor, GyroSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 import math
+import movement
 
 
 tank = MoveTank(OUTPUT_A, OUTPUT_D)
-
-
-def move(inches, power):
-    rotPerInch = 0.1395546
-
-    tank.on_for_rotations(
-            SpeedPercent(-power), SpeedPercent(-power), inches*rotPerInch
-            )
-def turn(degrees, power):
-    tank.turn_degrees(
-        speed=SpeedPercent(power),
-        target_angle=degrees,
-    )
-
-
 sound = Sound()
-
 gs = GyroSensor(INPUT_1)
 
+# Calibrate gyroscope
 sound.speak("Calibrating, do not touch mindstorm.")
 sleep(1)
 gs.calibrate()
 sound.speak("Calibration complete.")
 sleep(1)
-
 tank.gyro = gs
-dir = True
 
-for i in range(6):
-    move(10, 30)
-    turn(dir*360-180, 30)
-    dir = not dir
+# Subtask 1A
+movement.SubTask1A(30, 10, tank)
---
+++ movement.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor, GyroSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+import math
+
+def move(cm, power, tank):
+    cm *= 0.05494276
+
+    tank.on_for_rotations(
+            SpeedPercent(-power), SpeedPercent(-power), cm
+            )
+
+def turn(degrees, power, tank):
+    tank.turn_degrees(
+        speed=SpeedPercent(power),
+        target_angle=degrees,
+    )
+
+def SubTask1A(cm, laps, tank, power=30):
+    dir = True
+    for i in range(laps*2):
+        move(dir*(cm*2)-cm, power)
+        dir = not dir
