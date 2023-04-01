da2d9bf920ad568fa3967ba899fe95a8cf45feec
BenMac31 <benjaminmcintyre06@protonmail.com>
2023-04-01 19:53:20 -0400

    Fixed values in colorTest and added sound output.

--- colorTest.py
+++ colorTest.py
@@ -6,23 +6,28 @@ from time import sleep
 # from ev3dev2.sensor import INPUT_1
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D, MoveDifferential, SpeedRPM
 from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sound import Sound
 from ev3dev2.sensor import INPUT_1
 
 import display
 import movement
 
 tank = MoveTank(OUTPUT_A, OUTPUT_D)
+sound = Sound()
 cs = ColorSensor(INPUT_1)
 
 count = 0
 barVal = 0
 while count < 4:
-    movement.cartesian_move(0, 1.37*2.54, tank, [0, 0])
+    movement.cartesian_move(0, 0.5, tank, [0, 0])
     count += 1
     colVal = cs.value()
     if colVal > 30:
+        sound.speak("Black")
         barVal += 1*(2**count);
-    sleep(0.01)
+    else:
+        sound.speak("White")
+    sleep(0.5)
 
 display.displayBarCode(barVal);
 sleep(10);
