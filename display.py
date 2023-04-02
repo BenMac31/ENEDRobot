3c736f69eb5e77fbdde505966cf8d5e6bfb7ed3a
BenMac31 <benjaminmcintyre06@protonmail.com>
2023-04-02 10:24:07 -0400

    Gave display.py a size value to make more noticable on the small ev3 screen, and removed test run of function.

--- display.py
+++ display.py
@@ -4,12 +4,11 @@
 # from ev3dev2.sensor import INPUT_1
 
 
-def displayBarCode(number):
-    for binary in '{0:04b}'.format(number):
-        if binary == "1":
-            print("#", end='')
-        else:
-            print(" ", end='')
-    print()
-
-displayBarCode(5);
+def displayBarCode(number, size=3):
+    for i in range(size):
+        for binary in '{0:04b}'.format(number):
+            if binary == "1":
+                print("#"*size, end='')
+            else:
+                print(" "*size, end='')
+        print("\\n")
