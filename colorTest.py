21ca413cd74edcae10b6fabbe5daa2de9c6e5a39
BenMac31 <benjaminmcintyre06@protonmail.com>
2023-04-02 10:21:01 -0400

    Fixed color test

--- colorTest.py
+++ colorTest.py
@@ -4,29 +4,30 @@ from time import sleep
 
 # from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D
 # from ev3dev2.sensor import INPUT_1
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D, MoveDifferential, SpeedRPM
+from ev3dev2.motor import OUTPUT_A, MoveTank, OUTPUT_D
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sound import Sound
-from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_3
 
 import display
 import movement
 
 tank = MoveTank(OUTPUT_A, OUTPUT_D)
 sound = Sound()
-cs = ColorSensor(INPUT_1)
+cs = ColorSensor(INPUT_3)
 
 count = 0
 barVal = 0
 while count < 4:
-    movement.cartesian_move(0, 0.5, tank, [0, 0])
+    movement.cartesian_move(0, 0.5, tank, [0, 0], 20)
     count += 1
     colVal = cs.value()
+    sound.speak(colVal)
     if colVal > 30:
-        sound.speak("Black")
+    #     sound.speak("White")
         barVal += 1*(2**count);
-    else:
-        sound.speak("White")
+    # else:
+    #     sound.speak("Black")
     sleep(0.5)
 
 display.displayBarCode(barVal);
