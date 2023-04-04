440a2a19f4012a0d7feb251773ea10d63c8f8096
BenMac31 <benjaminmcintyre06@protonmail.com>
2023-04-04 05:44:48 -0400

    Initial commit

--- collisionAvoidanceTest.py
+++ collisionAvoidanceTest.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
 
 from time import sleep
+import threading
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, OUTPUT_D, MoveDifferential, SpeedRPM
 from ev3dev2.sensor import INPUT_1, INPUT_2
 from ev3dev2.sensor.lego import TouchSensor, GyroSensor, UltrasonicSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
-import math
-import movement
 from ev3dev2.wheel import EV3EducationSetRim
 
 
 tank = MoveTank(OUTPUT_A, OUTPUT_D)
+test = LargeMotor(OUTPUT_A)
 sound = Sound()
 us = UltrasonicSensor(INPUT_2)
+stopOnDetect = True
+distance = 999
 
-# Subtask 1A
-# movement.SubTask1A(30, 10, tank, gs) # Produces error of 4.4.166667cm x, 2.666667cm y
+def stopIfDistance():
+    while True:
+        distance = us.distance_inches
+        if stopOnDetect and distance[0] < 12:
+            tank.off()
+            sound.speak("Obstacle detected at " + str(distance) + " inches. Exiting program.")
+            quit()
 
-sound.speak("Testing careful movement, robot will move 10 ft forward and stop if there is an obstacle.")
-movement.careful_move(120, tank, us, sound)
+def readPositions():
+    while True:
+        pos = test.rotations
+        sound.speak(str((pos)/0.4313007) + "Inches")
+
+inch = 120
+
+"""Moves 'inch' inch forward (backwards is negative.)"""
+threading.Thread(target=stopIfDistance, args=(), daemon=True).start()
+threading.Thread(target=readPositions, args=(), daemon=True).start()
+tank.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 12*0.4313007)
+tank.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 24*0.4313007)
+tank.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 36*0.4313007)
+tank.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 48*0.4313007)
